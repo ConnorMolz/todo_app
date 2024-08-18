f129db50eb6784d948ccc254f18657dd56084c76
ConnorMolz <114417919+ConnorMolz@users.noreply.github.com>
2024-08-18 22:52:04 +0200

    Modify nativwind settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
+  content: [
+      "./App.{js,jsx,ts,tsx}",
+      "./app/**/*.{js,jsx,ts,tsx}", // Include all JS, JSX, TS, and TSX files in the app folder
+      "./components/**/*.{js,jsx,ts,tsx}", // Include all JS, JSX, TS, and TSX files in the components folder]
+  ],
   theme: {
-    extend: {},
+      extend: {
+          fontFamily: {
+              rmono: ["Roboto-Mono", "sans-serif"],
+          },
+      },
   },
-  plugins: [],
-}
+  plugins: [
 
+  ],
+};
