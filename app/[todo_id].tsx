4966661a748e4a93e5422011aee7e27b5edccedd
ConnorMolz <114417919+ConnorMolz@users.noreply.github.com>
2024-08-29 18:18:13 +0200

    Show it on existing Todos

--- app/[todo_id].tsx
+++ app/[todo_id].tsx
@@ -1,8 +1,10 @@
-import { View, Text, TextInput, Button, Alert } from 'react-native'
+import { View, Text, TextInput, Button, Alert, Modal } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { supabase } from '@/lib/supabase';
 import { Session } from '@supabase/supabase-js';
 import { router, useLocalSearchParams } from 'expo-router';
+import { CheckBox, Input } from 'react-native-elements';
+import { DataTable } from 'react-native-paper';
 
 const UpdateTodo = () => {
     const { todo_id } = useLocalSearchParams();
@@ -11,6 +13,12 @@ const UpdateTodo = () => {
     const [description, setDescription] = useState('');
     const [session, setSession] = useState<Session | null>(null)
     const [tableData, setTableData] = React.useState<{ entryName: string, entryDescription: string, entryChecked: boolean }[]>([]);
+    const [hasTable, setHasTable] = React.useState(false);
+
+    const [isPopUpOpen, setIsPopUpOpen] = React.useState(false);
+    const [entryName, setEntryName] = React.useState('');
+    const [entryDescription, setEntryDescription] = React.useState('');
+    const [entryChecked, setEntryChecked] = React.useState(false);
 
     useEffect(() => {
         supabase.auth.getSession().then(async ({ data: { session } }) => {
@@ -30,6 +38,9 @@ const UpdateTodo = () => {
          setTodo(data[0].todo);
          setDescription(data[0].description);
          setTableData(data[0].table);
+         if(data[0].table.length > 0) {
+             setHasTable(true);
+         }
     }
 
     function updateTodo(){
@@ -58,6 +69,28 @@ const UpdateTodo = () => {
         })
     }
 
+    function createTable() {
+        setHasTable(true);
+        console.log("WIP")
+    }
+
+    function openPopUp() {
+        setIsPopUpOpen(true);
+    }
+
+    function addTableItem() {
+        const newEntry = {
+            "entryName": entryName,
+            "entryDescription": entryDescription,
+            "entryChecked": entryChecked
+        };
+        setTableData([...tableData, newEntry]);
+        setIsPopUpOpen(false);
+        setEntryChecked(false);
+        setEntryName('');
+        setEntryDescription('');
+    }
+
 
     return (
         <View className='py-10 bg-neutral-700 flex-1'>
@@ -79,7 +112,60 @@ const UpdateTodo = () => {
                     numberOfLines={4}
                 />
         </View>
-        <View className='justify-between flex-row text-justify'>
+        {
+                !hasTable && <Button onPress={createTable} title="Add table"></Button>
+            }
+            {hasTable && 
+                <View className='justify-center py-10'>
+                    <DataTable>
+                        <DataTable.Header>
+                            <DataTable.Title><Text>Todo</Text></DataTable.Title>
+                            <DataTable.Title><Text>Description</Text></DataTable.Title>
+                            <DataTable.Title><Text>Done</Text></DataTable.Title>
+                            {/*<DataTable.Title> </DataTable.Title>*/}
+                        </DataTable.Header>
+                            {tableData.map((item, index) => {
+                                return (
+                                    <DataTable.Row key={index}>
+                                        <DataTable.Cell><Text>{item.entryName}</Text></DataTable.Cell>
+                                        <DataTable.Cell><Text>{item.entryDescription}</Text></DataTable.Cell>
+                                        <DataTable.Cell><CheckBox checked={item.entryChecked} onPress={() => item.entryChecked = !item.entryChecked} /></DataTable.Cell>
+                                        {/*<DataTable.Cell><Button icon={<Entypo name="trash" size={24} color="black" />} onPress={() =>{delete(tableData[index])}}></Button></DataTable.Cell>*/}
+                                    </DataTable.Row>
+                                )
+                            })}
+                    </DataTable >
+                    <Button onPress={openPopUp} title="Add item"></Button>
+                    <Modal
+                        animationType='slide'
+                        visible={isPopUpOpen}
+                        onRequestClose={() => addTableItem()}
+                        >
+                        <View>
+                            <Input placeholder="Enter your Entry" 
+                                value={entryName}
+                                onChangeText={setEntryName}
+                                className='bg-white p-2 m-2 scroll-py-10'
+                                maxLength={40}
+                                editable={true} />
+                            <Input placeholder="Enter description"
+                                value={entryDescription}
+                                onChangeText={setEntryDescription}
+                                className='bg-white p-2 m-2 scroll-py-10'
+                                editable={true}
+                                multiline={true}
+                                numberOfLines={4} />
+                            <CheckBox 
+                                title={"Done"}
+                                checked={entryChecked}
+                                onPress={() => setEntryChecked(!entryChecked)}
+                            />
+                            <Button onPress={addTableItem} title="Add item"></Button>
+                        </View>
+                    </Modal>
+                </View>
+            }
+        <View className='justify-between flex-row text-justify py-10'>
             <View className='' >
                 <Button title="Update Todo" onPress={updateTodo} />
             </View>
